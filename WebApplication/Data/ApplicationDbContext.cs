7b82e19e1cbd8300dfb762e241ce5f2729854218
Ranju Mathews <ransmin59@gmail.com>
2022-02-13 08:49:53 +0000

    Firts Commit

---
+++ WebApplication/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication.Models;
+
+namespace WebApplication.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options): base(options)
+        {
+
+        }
+        public DbSet<Category>Category { get; set; }
+    }
+}
